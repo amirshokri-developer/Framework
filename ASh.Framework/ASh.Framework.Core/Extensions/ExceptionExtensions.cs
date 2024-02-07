9fa85165f1fa5475356c3b56f60c73f2dafa2269
Amir.Shokri <Amir.Shokri@zoodfood.com>
2024-02-07 13:51:25 +0330

    add excption extensions

---
+++ ASh.Framework/ASh.Framework.Core/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace ASh.Framework.Core.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static void ReThrow(this Exception exception)
+        {
+            ExceptionDispatchInfo.Capture(exception).Throw();
+        }
+    }
+}
