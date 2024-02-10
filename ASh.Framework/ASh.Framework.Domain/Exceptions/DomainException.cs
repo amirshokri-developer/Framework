6e0644d84b260ac146014aa8d92a4b724aa0302b
Amir.Shokri <Amir.Shokri@zoodfood.com>
2024-02-10 09:52:43 +0330

    Add Domain Exception

---
+++ ASh.Framework/ASh.Framework.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿namespace ASh.Framework.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public int? Code { get; }
+        public DomainException(string message, int? code = null) : base(message) 
+        {
+            Code = code;
+        }
+    }
+}
